398319ba57c05462d5dcaab2dfdd805f3c2999cf
Dasha <ponchik725007@gmail.com>
2012-06-27 14:47:17 +0400

    hhh22

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+  include_once('functions.php');
+ 
+ 
+ 
+ 
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="keywords" content="МГТУ ГА, кафедра, прикладной математики, контакты, история" />
+		<meta name="description" content="Информация о кафедре прикладной математики МГТУ ГА" />
+		<link media="screen" rel="stylesheet" href="style.css"/>
+		<title>Кафедра прикладной математики</title>
+	</head>
+
+	<body>
+		
+		<div id="container">
+			<div id="header">
+				<?hat();?>
+				<p id="z">Главная страница
+				</p>
+			</div>
+			<div id="lft">
+				<?menu();?>
+			</div>
+			
+			<div id="rght">
+				<?kont();?>			
+			</div> 
+			<div id="cntr"> 
+				<div id="std">
+					<p><h2><u>
+					Упс. Пока ничего не случилось)))
+					</u></h2></p>
+				
+					
+					<span class="sss"><a href="#here">В начало</a></span><br>
+				</div>	
+			</div>
+			
+			<div id="footer">
+				<?floorr();?>				
+			</div>
+		</div>
+	</body>
+</html>
