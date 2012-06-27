ddf1fc8aa595a2c472a4ec88403dcff2fa348f28
Dasha <ponchik725007@gmail.com>
2012-06-27 15:20:56 +0400

    nado izmenit` logotip kafedry

--- functions.php
+++ functions.php
@@ -1,20 +1,20 @@
 <?php 
 function menu(){?>
 	<ul id="head">
-	<li><a href="index.php" title="Главная страница">Главная</a></li>
-	<li><a href="stud.php" title="Расписание. Рейтинг. Гранты. Аспирантура.">Студентам</a></li>
-	<li><a href="abit.php" title="Описание специальности. Вступительные испытания">Абитуриентам</a></li>
-	<!--li><a href="news.php"title="Новости">Новости</a></li-->
-	<li><a href="sost.php" title="Наши преподаватели">Преподавательский состав</a></li>
-	<li><a href="dis.php" title="Изучаемые дисциплины">Дисциплины</a></li>
-	<li><a href="nayka.php" title="Научная деятельность">Научная деятельность</a></li>
-	<li><a href="history.php" title="История кафедры">История кафедры</a></li>
-	<li><a href="sotr.php" title="С кем сотрудничаем">Сотрудничество</a></li>
-	
-	<li><a href="kontakt.php" title="Адрес и контактные телефоны">Контакты</a></li>		
-	<li><a href="res.php" title="Полезные ресурсы">Полезные ресурсы</a></li>	
-	<li><a href="http://kafedra.forum2x2.ru/login" title="Форум кафедры ПМ"  target="_blank">Форум кафедры ПМ</a></li>
-</ul>
+		<li><a href="index.php" title="Главная страница">Главная</a></li>
+		<li><a href="stud.php" title="Расписание. Рейтинг. Гранты. Аспирантура.">Студентам</a></li>
+		<li><a href="abit.php" title="Описание специальности. Вступительные испытания">Абитуриентам</a></li>
+		<!--li><a href="news.php"title="Новости">Новости</a></li-->
+		<li><a href="sost.php" title="Наши преподаватели">Преподавательский состав</a></li>
+		<li><a href="dis.php" title="Изучаемые дисциплины">Дисциплины</a></li>
+		<li><a href="nayka.php" title="Научная деятельность">Научная деятельность</a></li>
+		<li><a href="history.php" title="История кафедры">История кафедры</a></li>
+		<li><a href="sotr.php" title="С кем сотрудничаем">Сотрудничество</a></li>
+		
+		<li><a href="kontakt.php" title="Адрес и контактные телефоны">Контакты</a></li>		
+		<li><a href="res.php" title="Полезные ресурсы">Полезные ресурсы</a></li>	
+		<li><a href="http://kafedra.forum2x2.ru/login" title="Форум кафедры ПМ"  target="_blank">Форум кафедры ПМ</a></li>
+	</ul>
 <?}
 
 function hat(){?>
@@ -22,20 +22,22 @@ function hat(){?>
 		<tr>
 			<td>
 				<a href="index.php" title="Перейти на главную страницу">
-					<img src="img/logo.png"  alt="Логотип института" >
+					<img src="img/logok.jpg"  alt="Логотип кафедры" >
 				</a>
-				<a name="here"></a>
-			</td>                        
-			<td align="right">
+			</td>
+			                       
+			<td align="right" width="70%">
 				<a href="index.php" title="Перейти на главную страницу">
 					МГТУ ГА - Кафедра прикладной математики
 				</a>
 			
 			</td>	
-			<!--td>
-				<img src="img/logok.jpg"  alt="Логотип института" >
-				
-			</td-->
+			<td width="50%">
+				<a href="index.php" title="Перейти на главную страницу">
+					<img src="img/logo.png"  alt="Логотип института" >
+				</a>
+				<a name="here"></a>
+			</td> 
 		</tr>
 	</table>			
 <?}
